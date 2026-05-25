ca9dbe9d1be5754f29085a4f377488ef32af0a11
erikagiella <erikagiella93@gmail.com>
2026-05-25 16:36:00 +0200

    stilizzazione card pt1

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -11,25 +11,22 @@ export default function Cards() {
     }
 
     return (
-        <>
-            <h1>Learn Web development</h1>
-            <div className="cards">
+        <div className="container">
+            <h1 className="text-center">Learn Web development</h1>
+            <div className="d-flex flex-column">
                 {languages.map(language => (
-                    <div className="item" key={language.id}>
-                        <button onClick={() => toggleLanguage(language.id)}>{language.title}</button>
+                    <div className="card" key={language.id}>
+                        <button className={activeId === language.id ? "btn btn-warning" : "btn btn-primary"} onClick={() => toggleLanguage(language.id)}>{language.title}</button>
                         {
                             activeId === language.id &&
                             (
-                                <div className="card">
-                                    <div className="card-title">{language.title}</div>
-                                    <div className="card-text">{language.description}</div>
-                                </div>
+                                <div className="card-text">{language.description}</div>
                             )
                         }
                     </div>
                 ))}
             </div>
-        </>
+        </div>
     )
 
 }
