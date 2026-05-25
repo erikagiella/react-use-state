ec641a8ccbe8824520728ad26aeddd18ed49e31f
erikagiella <erikagiella93@gmail.com>
2026-05-25 16:01:08 +0200

    ToggleLanguage e useState su id

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,15 +2,30 @@ import { useState } from "react";
 import languages from "../assets/languages.js"
 
 export default function Cards() {
+
+    const [activeId, setActiveId] = useState();
+
+
+    function toggleLanguage(id) {
+        setActiveId(id);
+    }
+
     return (
         <>
             <h1>Learn Web development</h1>
             <div className="cards">
                 {languages.map(language => (
-                    <div className="card" key={language.id}>
-                        <button>{language.title}</button>
-                        <div className="card-title">{language.title}</div>
-                        <div className="card-text">{language.description}</div>
+                    <div className="item" key={language.id}>
+                        <button onClick={() => toggleLanguage(language.id)}>{language.title}</button>
+                        {
+                            activeId === language.id &&
+                            (
+                                <div className="card">
+                                    <div className="card-title">{language.title}</div>
+                                    <div className="card-text">{language.description}</div>
+                                </div>
+                            )
+                        }
                     </div>
                 ))}
             </div>
